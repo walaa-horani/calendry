142916f0da830e51d253fc376a09049eed131988
Your Name <walaa.horani@hotmail.com>
2026-04-29 20:37:04 +0300

    feat(sanity): wire up userType + availabilityType in schema index

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,19 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { userType } from './documents/userType'
+import { availabilityType } from './documents/availabilityType'
+
+import { timeInterval } from './objects/timeInterval'
+import { daySchedule } from './objects/daySchedule'
+import { location } from './objects/location'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    // Documents
+    userType,
+    availabilityType,
+    // Objects
+    timeInterval,
+    daySchedule,
+    location,
+  ],
 }
